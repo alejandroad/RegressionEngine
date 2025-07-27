df09d2a34aa518515ef17bfd8643524016de6031
Alejandro Arias <alejandroarias@Alejandros-MacBook-Air.local>
2025-07-27 15:43:46 -0500

    added python script to read csv data and write to file

---
+++ py_utils/data_cleaner.py
@@ -0,0 +1,22 @@
+import csv
+
+input_file = '../data/full_weather_data.csv'
+output_file = '../data/normalized_data.csv'
+
+fields_to_keep = ["Date", "MaxTemp", "MinTemp", "WindGustSpd", "Precip"]
+
+with open(input_file, newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    normalized_rows = []
+
+    for row in reader:
+        if not row["Date"] or not row["MaxTemp"] or not row["MinTemp"]:
+            continue
+
+        normalized_row = {field: row.get(field, '') for field in fields_to_keep}
+        normalized_rows.append(normalized_row)
+
+with open(output_file, 'w', newline='') as csvfile:
+    writer = csv.DictWriter(csvfile, fieldnames=fields_to_keep)
+    writer.writeheader()
+    writer.writerows(normalized_rows)
