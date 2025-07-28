1c3046a4a450a96fd4fa3e31495c3b16fd45aab1
Alejandro Arias <alejandroarias@Alejandros-MacBook-Air.local>
2025-07-28 13:28:00 -0500

    regression engine header file

---
+++ src/regression_engine.hpp
@@ -0,0 +1,27 @@
+#ifndef REGRESSION_ENGINE_HPP
+#define REGRESSION_ENGINE_HPP
+
+#include "regression_utils.hpp"
+
+class RegressionEngine {
+
+    std::vector<DataPoint>& dataset;
+
+    float mse;
+    float slope;
+    float intercept;
+    bool trained;
+
+    public:
+        RegressionEngine(const std::vector<DataPoint>& dataset);
+        void train();
+        float predict(float x) const;
+        void compute_mse() const;
+        float compute_r_squared() const;
+        void change_dataset(const std::vector<DataPoint>& dataset);
+        void print_model();
+        void save_model(const std::string& path);
+        void load_model(...);
+};
+
+#endif
