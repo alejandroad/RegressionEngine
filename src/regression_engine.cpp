ea7aa639cf51cb811dff5618936c5ee26c87a46b
Alejandro Arias <alejandroarias@alejandros-air.mynetworksettings.com>
2025-07-29 00:01:04 -0500

    refactored RegressionEngine contructor

--- src/regression_engine.cpp
+++ src/regression_engine.cpp
@@ -1,4 +1,6 @@
 #include "regression_utils.hpp"
 #include "regression_engine.hpp"
 
-RegressionEngine::RegressionEngine(std::vector<DataPoint>& datasetReference) : dataset(datasetReference) , mse(0), slope(0), intercept(0), trained(false) {}
+RegressionEngine::RegressionEngine(std::vector<DataPoint> &datasetReference) : dataset(datasetReference),
+                                                                               mse(0), slope(0), intercept(0),
+                                                                               trained(false) {}
